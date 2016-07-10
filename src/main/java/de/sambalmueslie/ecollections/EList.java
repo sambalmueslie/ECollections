7a3eaaa8992b0ea9ce5b71715091ad6981c3f6d7
sambalmueslie <github@sambalmueslie.de>
2016-07-10 13:20:48 +0200

    add create method for list

--- src/main/java/de/sambalmueslie/ecollections/EList.java
+++ src/main/java/de/sambalmueslie/ecollections/EList.java
@@ -1,7 +1,15 @@
 package de.sambalmueslie.ecollections;
 
+import java.util.Arrays;
 import java.util.List;
 
+import de.sambalmueslie.ecollections.internal.EFactory;
+
 public interface EList<E> extends ECollection<E>, List<E> {
+
+	@SafeVarargs
+	static <T> EList<T> list(T... elements) {
+		return EFactory.decorate(Arrays.asList(elements));
+	}
 
 }
